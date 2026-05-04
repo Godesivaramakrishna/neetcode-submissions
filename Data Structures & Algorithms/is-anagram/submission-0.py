b079cb771b764b2d86a41cfe042b8e6ca940cdd6
Godesivaramakrishna Durgaprasad <23MH1A0424@acoe.edu.in>
2026-05-04 20:09:29 +0530

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        freq = {}
+        for ch in s:
+            freq[ch] = freq.get(ch,0)+1
+        for ch in t:
+            freq[ch] = freq.get(ch,0)-1
+        for val in freq.values():
+            if val != 0:
+                return False
+        return True
+        
